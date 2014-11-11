7a7ae23ef00ba21fc23fe8923359158cba0c3c90
Andrew Lilja <andrewlilja@gmail.com>
2014-11-11 13:08:16 -0600

    config file added

--- opaline.py
+++ opaline.py
@@ -1,15 +1,51 @@
+from yaml import load
+
 from window import Window
 from input_types import InputFile
 
+from pprint import pprint
+
 class Opaline:
-    def __init__(self, input_type, filename):
+    def __init__(self, input_type="", **kwargs):
         self.input_type = input_type
-        if input_type is "STREAM":
+        with open(kwargs.get('config',"config.yaml")) as f:
+            config_info = load(f)
+        if input_type == "":
+            if "input_type" in config_info.keys():
+                input_type = config_info['input_type'].upper()
+            else:
+                raise ValueError, "No input type found."
+        if input_type == "STREAM":
             #self.data_object = stream()
             pass
-        elif input_type is "FILE":
-            self.data_object = InputFile(filename,{"sec":"time", "CH40":"bp", "CH46":"rr"},separator="\t")
+        elif input_type == "FILE":
+            filename = kwargs.get('filename')
+            if filename is None:
+                if "input_file" in config_info.keys():
+                    filename = config_info['input_file']
+                else:
+                    raise ValueError, "No filename entered."
+            self.data_object = InputFile(
+                    filename,
+                    kwargs.get('channels',{"sec":"time", "CH40":"bp", "CH46":"rr"}),
+                    separator=kwargs.get('separator',','),
+                )
         else:
             raise ValueError, "Unknown input type \"%s\"." % input_type
 
-    
+    def _build_second_data(self, channel_data):
+        start_index = 0
+        output = []
+        for index, time in enumerate(channel_data['time']):
+            if time % 1 == 0 and time != 0:
+                output.append((
+                    channel_data['bp'][start_index:index+1],
+                    channel_data['rr'][start_index:index+1],
+                    channel_data['time'][start_index:index+1],
+                    ))
+                start_index = index
+        return output
+
+op = Opaline(filename="data/brs_250.txt", separator='\t')
+seconds = op._build_second_data(op.data_object.channel_data)
+print(len(seconds[0][0]))
--- test.py
+++ test.py
@@ -20,6 +20,44 @@ class TestWindow(unittest.TestCase):
         self.assertEqual(len(Window(range(0,100), width=3, overlap=2, iterations=6)), 8)  # odd window,  even overlap
         self.assertEqual(len(Window(range(0,100), width=4, overlap=2, iterations=4)), 10) # even window, even overlap
 
+    def test_list_of_tuples(self):
+        a = [
+            (0, 1, 2),
+            (2, 3, 4),
+            (4, 5, 6),
+            (6, 7, 8),
+            (8, 9, 10),
+            (10, 11, 12),
+            (12, 13, 14),
+            (14, 15, 16),
+            (16, 17, 18),
+            (18, 19, 20),
+            (20, 21, 22),
+            (22, 23, 24),
+            (24, 25, 26),
+            (26, 27, 28),
+            (28, 29, 30),
+            (30, 31, 32),
+            (32, 33, 34),
+            (34, 35, 36),
+            (36, 37, 38),
+            (38, 39, 40),
+            (40, 41, 42),
+            (42, 43, 44),
+            (44, 45, 46),
+            (46, 47, 48),
+            (48, 49, 50),
+            (50, 51, 52),
+            (52, 53, 54),
+            (54, 55, 56),
+            (56, 57, 58),
+            (58, 59, 60),
+            ]
+
+        window = Window(a, width=3, overlap=2)
+
+        # finish me with __getitem__ in window function
+
 class TestInputFile(unittest.TestCase):
     def test_processing(self):
         input_file = InputFile("test/test_data.txt",{"sec":"time", "CH40":"bp", "CH46":"rr"})
